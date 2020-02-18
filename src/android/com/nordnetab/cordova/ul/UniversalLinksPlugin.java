4e1114ec39a49609e9eca793647a7a9b10bd835e
andres vasquez <andresvas1995@outlook.com>
2020-02-18 14:27:32 -0500

    Update UniversalLinksPlugin.java

--- src/android/com/nordnetab/cordova/ul/UniversalLinksPlugin.java
+++ src/android/com/nordnetab/cordova/ul/UniversalLinksPlugin.java
@@ -32,7 +32,7 @@ public class UniversalLinksPlugin extends CordovaPlugin {
 
 
     // name host
-    private  final  String DEEP_LINK_HOST = "www.grupobancolombia.com";
+    private  final  String DEEP_LINK_HOST = "www.grupobancolombia.com/wps/portal/personas";
     private final  String SCHEME = "https";
 
     // list of hosts, defined in config.xml
